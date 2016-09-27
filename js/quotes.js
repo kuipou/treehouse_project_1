bdfce9a30df30d84cc61d43945df96af9c1a4b58
Christophe Rudyj <kuipou@gmail.com>
2016-09-27 11:12:46 -0400

    Added the getRandom func + Added the entire instruction in Readme

---
+++ js/quotes.js
@@ -0,0 +1,96 @@
+// Random quotes 
+
+// {
+//   quote: ,
+//   author: ,
+//   citation: ,
+//   year:  
+// }
+
+var quotes = [
+{
+  quote: "You want weapons? We're in a library. Books are the best weapon in the world. This room's the greatest arsenal we could have. ",
+  author: "The Tenth Doctor",
+  citation: "Doctor Who" ,
+  year: 2010
+},
+{
+  quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+  author: "Albert Einstein",
+  citation: "",
+                                                                                            
+},
+{
+  quote: "He looked at her the way all women want to be looked at by a man",
+  author: "F. Scott Fitzgerald",
+  citation: "The Great Gatsby",
+  year: 1924
+},
+{
+  quote: "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light",
+  author: "J.K. Rowling",
+  citation:"" ,
+  year: 2001
+}
+// ,
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// },
+// {
+//   quote: "",
+//   author: ,
+//   citation: ,
+//   year:  
+// }
+];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// getRandom generates a random number from [arg] and floor it then
+// returns it
+function getRandom(arg) {
+  return Math.floor(Math.random() * arg ) + 1
+}
+
+function printQuote() {}
+console.log(getRandom(quotes.length));
