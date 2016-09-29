be28d6c253b74a8afd211a9339a41766eeb5b47c
Christophe Rudyj <kuipou@gmail.com>
2016-09-29 16:08:10 -0400

    Added more quotes and made getRandom

--- js/quotes.js
+++ js/quotes.js
@@ -29,40 +29,41 @@ var quotes = [
 {
   quote: "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light",
   author: "J.K. Rowling",
-  citation:"" ,
+  citation:"Harry Potter " ,
   year: 2001
-}
-// ,
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
+},
+{
+  quote: "Blessed are the hearts that can bend; they shall never be broken.",
+  author: "Albert Camus",
+},
+{
+  quote: "Don't cry because it's over, smile because it happened",
+  author: "Doctor Seuss",
+},
+{
+  quote: "I fell in love the way you fall asleep: slowly, and then all at once",
+  author: "John Green",
+  citation: "The Fault in Our Stars",
+  year: 2012
+},
+{
+  quote: "Some infinities are bigger than other infinities",
+  author: "John Green",
+  citation: "The Fault in Our Stars",
+  year: 2012
+},
+{
+  quote: "Of course it is happening inside your head, Harry, but why on earth should that mean that it is not real?" ,
+  author: "J.K. Rowling",
+  citation: "Harry Potter and the Deathly Hallows",
+  year: 2007
+},
+{
+  quote: "It is the unknown we fear when we look upon death and darkness, nothing more.",
+  author: "J.K. Rowling",
+  citation: "Harry Potter and the the Half-Blood Prince" ,
+  year: 2006
+}//,
 // {
 //   quote: "",
 //   author: ,
--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// getRandom generates a random number from [arg] and floor it then
+// getRandom generates a random number from [arg] and floors it then
 // returns it
-function getRandom(arg) {
-  return Math.floor(Math.random() * arg ) + 1
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + 0;
 }
 
 function printQuote() {}
-console.log(getRandom(quotes.length));
+console.log("You currently have " + quotes.length + " quotes");
+// DEBUG REMOVE WHEN DONE 
+for ( var i = 0; i <= 100; i += 1){
+  console.log(getRandom(i, quotes.length));
+}
+
