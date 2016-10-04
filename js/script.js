4a1fe20d60331bbd48ba9092bb44e68352ebdfcf
Christophe Rudyj <kuipou@gmail.com>
2016-10-03 20:27:05 -0400

    made the RGB function plus created the printquote with rand bg

--- js/script.js
+++ js/script.js
@@ -5,13 +5,36 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 // getRandom generates a random number from [arg] and floors it then
 // returns it
 function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + 0;
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-function printQuote() {}
-console.log("You currently have " + quotes.length + " quotes");
-// DEBUG REMOVE WHEN DONE 
-for ( var i = 0; i <= 100; i += 1){
-  console.log(getRandom(i, quotes.length));
+function getRandomRGB() {
+  return "rgb(" + getRandom(0, 255) + "," + getRandom(0, 255) + "," + getRandom(0, 255) + ")";
 }
+// selects a random quote object from the quotes array
+// returns the randomly selected quote object
+function getRandomQuote() {
+ return query = quotes[Math.floor(Math.random() * quotes.length)];
+
+}
+
+function printQuote() {
+  var query = getRandomQuote();
+  var html =  '<p class="quote">' + query.quote + '</p> <p class="source">' + query.author;
+  if (typeof query.citation !== "undefined" ){
+      html += '<span class="citation">' + query.citation + '</span>';
+  }
+  if (typeof query.year !== "undefined" ) {
+      html += '<span class="year">' + query.year + '</span>';
+  }
+      html += '</p>';
+      document.body.style.backgroundColor = getRandomRGB();
+     document.getElementById('quote-box').innerHTML = html ;
+}
+var returnquote = getRandomQuote();
+console.log("You currently have " + quotes.length + " quotes" + "query object:"  );
+console.log("current quote from author " + returnquote.year + ": " + returnquote.quote );// DEBUG REMOVE WHEN DONE 
+// for ( var i = 0; i <= 10; i += 1){
+//   console.log(getRandom(i, quotes.length));
+// }
 
