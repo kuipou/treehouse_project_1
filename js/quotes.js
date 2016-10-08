e9d483662ac1ca4775e2c8294503723b193c37f5
Christophe Rudyj <kuipou@gmail.com>
2016-10-07 21:38:13 -0400

    Total of 31 quote plus added the fixed background color and final

--- js/quotes.js
+++ js/quotes.js
@@ -28,8 +28,8 @@ var quotes = [
 },
 {
   quote: "Happiness can be found, even in the darkest of times, if one only remembers to turn on the light",
-  author: "J.K. Rowling 1",
-  citation:"Harry Potter " ,
+  author: "J.K. Rowling",
+  citation:"Harry Potter and the Philosopher stone " ,
   year: 2001
 },
 {
@@ -63,35 +63,117 @@ var quotes = [
   author: "J.K. Rowling 2",
   citation: "Harry Potter and the the Half-Blood Prince" ,
   year: 2006
-}//,
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// },
-// {
-//   quote: "",
-//   author: ,
-//   citation: ,
-//   year:  
-// }
+},
+{
+  quote: "I didn’t write them a happy ending because 17-year-olds don’t get endings. They get beginnings.",
+  author: "Rainbow Rowell",
+  year: 2015  
+},
+{
+  quote: "People tend to find books when they are ready for them.",
+  author: "Neil Gaiman"  
+},
+{
+  quote: "My advice to other disabled people would be, concentrate on things your disability doesn't prevent you doing well, and don't regret the things it interferes with. Don't be disabled in spirit as well as physically.",
+  author: "Stephen Hawking"
+},
+{
+  quote: "There is a fundamental difference between religion, which is based on authority, [and] science, which is based on observation and reason. Science will win, because it works.",
+  author: "Stephen Hawking",
+  citation: "To Diane Sawyer/ABC News",
+  year: 2010
+},
+{
+  quote: "Next time someone complains that you have made a mistake, tell him that may be a good thing. Because without imperfection, neither you nor I would exist.",
+  author: "Stephen Hawking",
+  citation: "Into the Universe with Stephen Hawking",
+  year: 2010 
+},
+{
+  quote: "The best love is the kind that awakens the soul and makes us reach for more, that plants a fire in our hearts and brings peace to our minds. And that's what you've given me. That's what I'd hoped to give you forever",
+  author: "Nicholas Sparks",
+},
+{
+  quote: "I cannot fix on the hour, or the spot, or the look or the words, which laid the foundation. It is too long ago. I was in the middle before I knew that I had begun.",
+  author: "Jane Austen",
+  citation: "Pride and Prejudice",
+  year: 1813
+},
+{
+  quote: "I stood still, vision blurring, and in that moment, I heard my heart break. It was a small, clean sound, like the snapping of a flower's stem." ,
+  author: "Diana Gabaldon",
+  citation: "Dragonfly in Amber",
+  year: 1992
+},
+{
+  quote: "It's really hard to design products by focus groups. A lot of times, people don't know what they want until you show it to them.",
+  author: "Steve Jobs",
+  citation: "Bloomberg Bussiness",
+  year: 1998  
+},
+{
+  quote: "That’s been one of my mantras — focus and simplicity. Simple can be harder than complex; you have to work hard to get your thinking clean to make it simple",
+  author: "Steve Jobs",
+},
+{
+  quote: "Being the richest man in the cemetery doesn’t matter to me … Going to bed at night saying we’ve done something wonderful … that’s what matters to me.",
+  author: "Steve Jobs",
+  citation: "The Wall Street Journal",
+  year: 1998
+},
+{
+  quote: "It's better to be a pirate than join the Navy.",
+  author: "Steve Jobs",
+  citation: "Odyssey: Pepsi to Apple", 
+},
+{
+  quote: "Before you were born, your parents weren't as boring as they are now. They got that way from paying your bills, cleaning your clothes and listening to you talk about how cool you thought you were. So before you save the rain forest from the parasites of your parent's generation, try delousing the closet in your own room.",
+  author: "Bill Gates",
+},
+{
+  quote: "Don't compare yourself with anyone in this world...if you do so, you are insulting yourself.",
+  author: "Bill Gates"
+},
+{
+  quote: "Someone once said that death is not the greatest loss in life. The greatest loss is what dies inside of us while we live. I could tell you who said it, but who the hell really cares.",
+  author: "Haley James Scott",
+  citation: "One Tree Hill"
+},
+{
+  quote: "Live long, and prosper.",
+  author: "Unknown",
+  citation: "Vulcan Proverb"
+},
+{
+  quote: "…Second star to the right — and straight on ’til morning.",
+  author: "James T. Kirk",
+  citation: "Star Trek VI: Undiscovered Country",
+  year: 1991 
+},
+{
+  quote: "Mister Worf, dispatch a subspace message to Admiral Hanson. We have engaged the Borg.",
+  author: "Jean-Luc Picard",
+  citation: "The Best of Both Worlds (Star Trek The Next Generation)",
+  year: 1990,
+  bgc: "rgb(153, 3, 19)"
+},
+{
+  quote: "I always believed that I didn't need children to complete my life. Now I couldn't imagine life without them.",
+  author: "Jean-Luc Picard",
+  citation: "The Inner Light (Star Trek The Next Generation)",
+  year: 1992,
+  bgc: "rgb(153, 3, 19)"
+},
+{
+  quote: "At this moment there are 6,470,818,671 people in the world. Some are running scared. Some are coming home. Some tell lies to make it through the day. Others are just now facing the truth. Some are evil men, at war with good. And some are good, struggling with evil. Six billion people in the world. Six billion souls. And sometimes -- all you need is one",
+  author: "Peyton Sawyer",
+  citation: "One Tree Hill",
+  year: 2005
+},
+{
+  quote: "Sometimes when you look back at your life certain moments stand out: the day you graduated high school, your wedding day, the day you found out you were going to be a father. Most of the time they happen in that order. Most of the time.",
+  author: "Lucas Scott",
+  citation: "One Tree Hill",
+  year: 2007
+}
 ];
--- js/script.js
+++ js/script.js
@@ -9,8 +9,12 @@ function getRandom(min, max) {
 }
 
 function getRandomRGB() {
-  return "rgb(" + getRandom(0, 255) + "," + getRandom(0, 255) + "," + getRandom(0, 255) + ")";
+  // The rgb value is randomized to be sure that when called the color woudn't be repeated
+  // how ever the random is up to 200 because upon testing the colors would end up too bright 
+  return "rgb(" + getRandom(0, 200) + "," + getRandom(0, 200) + "," + getRandom(0, 200) + ")";
 }
+
+
 // selects a random quote object from the quotes array
 // returns the randomly selected quote object
 function getRandomQuote() {
@@ -19,22 +23,33 @@ function getRandomQuote() {
 }
 
 function printQuote() {
+  // The query var stores the Random quote
   var query = getRandomQuote();
+
+  // the var html starts the construct of the string
   var html =  '<p class="quote">' + query.quote + '</p> <p class="source">' + query.author;
+
+  // we check if both citation and year separatly aren't empty if they aren't display span
   if (typeof query.citation !== "undefined" ){
       html += '<span class="citation">' + query.citation + '</span>';
   }
   if (typeof query.year !== "undefined" ) {
       html += '<span class="year">' + query.year + '</span>';
   }
+  // close the string
       html += '</p>';
+
+      // randomize the BG color via the getRandomRGB function
+      // if the quote has a specific color grab it
+      if (typeof query.bgc !== "undefined" ){
+        document.body.style.backgroundColor = query.bgc;
+      } else {
       document.body.style.backgroundColor = getRandomRGB();
+        }
+     // modify the quote box div with our string
      document.getElementById('quote-box').innerHTML = html ;
 }
-var returnquote = getRandomQuote();
-console.log("You currently have " + quotes.length + " quotes" + "query object:"  );
-console.log("current quote from author " + returnquote.year + ": " + returnquote.quote );// DEBUG REMOVE WHEN DONE 
-// for ( var i = 0; i <= 10; i += 1){
-//   console.log(getRandom(i, quotes.length));
-// }
+
+//end
+console.log(0);
 
